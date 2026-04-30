941f12dfd4f5774d924c8cbe42d27a0e32d9e82d
Sofilayerdi <sofilayerdi@gmail.com>
2026-04-30 13:51:01 -0600

    handler productos

--- backend/handlers/productos.go
+++ backend/handlers/productos.go
@@ -57,3 +57,45 @@ func ListarProductos(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(productos)
 
 }
+
+func ListarProductosCategoria(w http.ResponseWriter, r *http.Request) {
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	if limit == 0 {
+		limit = 5
+	}
+
+	page, _ := strconv.Atoi(r.URL.Query().Get("page"))
+	if page == 0 {
+		page = 1
+	}
+
+	offset := limit * (page - 1)
+
+	query := "SELECT p.id_producto, p.nombre, p.categoria, p.precio, pr.nombre AS proveedor 
+				FROM producto p 
+				LEFT JOIN proveedor pr ON p.id_proveedor = pr.id_proveedor
+				LIMIT ? OFFSET ?;"
+
+	rows, err := db.Query(query, limit, offset)
+	if err != nil {
+		http.Error(w, "Error al obtener los productos", http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var productos []Productos
+	for rows.Next() {
+		var s Producto
+		err := rows.Scan(&s.IDProducto, &s.Nombre, &s.Categoria, &s.Precio, &s.Proveedor)
+		if err != nil {
+			http.Error(w, "Error leyendo datos", http.StatusInternalServerError)
+			return
+		}
+		productos = append(productos, s)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(productos)
+
+}
+
